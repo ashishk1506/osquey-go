b6b1c72dcc5e96ce3854a487fc153e9c1f4466c8
ashish <ashishk8581@gmail.com>
2025-04-06 00:09:37 +0530

    add: osquery cli cmd

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os/exec"
+	"osquey/db"
+)
+
+type OsVersion struct {
+	Version string `json:"version"`
+}
 
 func main() {
 	fmt.Println("Hello World")
+	db.DB()
+
+	// os version
+	cmd := exec.Command("osqueryi", "--json", "SELECT version FROM os_version LIMIT 1;")
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Error running osqueryi: %v\nOutput: %s", err, output)
+	}
+
+	var verObj []OsVersion
+
+	err = json.Unmarshal(output, &verObj)
+	if err != nil {
+		log.Fatalf("Error unmarshalling JSON output: %v", err)
+	}
+
+	fmt.Printf("%+v\n", verObj)
+
 }
